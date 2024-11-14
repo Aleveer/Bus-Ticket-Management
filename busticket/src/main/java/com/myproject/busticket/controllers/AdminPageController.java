bfa6a57fb80d3aa54efdb7982a91ad3370cf9627
phuhieu1505 <51695500+phuhieu1505@users.noreply.github.com>
2024-11-14 17:44:19 +0700

    update file addTrip

--- busticket/src/main/java/com/myproject/busticket/controllers/AdminPageController.java
+++ busticket/src/main/java/com/myproject/busticket/controllers/AdminPageController.java
@@ -1,19 +1,32 @@
 package com.myproject.busticket.controllers;
 
+import com.myproject.busticket.models.Trip;
+import com.myproject.busticket.services.TripService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/admin")
 @Controller
 public class AdminPageController {
+    TripService tripService;
+    public AdminPageController(TripService tripService){
+        this.tripService = tripService;
+    }
     @GetMapping("/dashboard")
     public String dashBoardPage(){
         return "admin";
     }
 
     @GetMapping("/addTrip")
-    public String addTripPage(){
+    public String addTripPage(Model model){
+//        List<Trip> trips = tripService.findAll();
+//        model.addAttribute("trips",trips);
         return "addTrip";
     }
+
+
 }
